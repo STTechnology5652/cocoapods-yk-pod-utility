945cf1703788ba48cce190687340033741a9fd96
stephenchen <stephenchen.email>
2023-12-02 01:18:28 +0800

    chore: update version to 0.0.17

--- lib/cocoapods-ykutility/gem_version.rb
+++ lib/cocoapods-ykutility/gem_version.rb
@@ -1,5 +1,5 @@
 module CocoapodsYkPodUtility
-  VERSION = '0.0.16'
+  VERSION = '0.0.17'
   YK_POD_TEMPLATE_PATH = File.expand_path(File.join(__FILE__, '../../../podTemplate'))
   YK_POD_BUSINESS_REGISTER_PATH = File.expand_path(File.join(__FILE__, '../../../podTemplate/register'))
 end
