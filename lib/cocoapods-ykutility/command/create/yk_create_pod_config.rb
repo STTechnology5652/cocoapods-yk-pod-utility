34942c483db23f6e1b2ac46ca9d6aae6e8c93895
stephenchen <stephenchen.email>
2023-12-02 01:11:05 +0800

    fix: crash while not set git global user name and email

--- lib/cocoapods-ykutility/command/create/yk_create_pod_config.rb
+++ lib/cocoapods-ykutility/command/create/yk_create_pod_config.rb
@@ -1,23 +1,27 @@
 # frozen_string_literal: true
 
 module YKPod
-
   class YKCreatePodConfig
     attr_accessor :name, :language, :with_demo, :with_register, :author, :author_email, :prefix, :path
 
-    def initialize()
-      @name = ""
-      @language = ""
+    def initialize
+      @name = ''
+      @language = ''
       @with_demo = true
       @with_register = false
-      @author = open("|git config --global user.name").gets.strip.gsub('.', '')
-      @author_email = open("|git config --global user.email").gets.strip
-      @prefix = "YK"
+
+      user_name = open('|git config --global user.name').gets
+      user_name = user_name.blank? ? 'xxximac' : user_name
+      user_email = open('|git config --global user.email').gets
+      user_email = user_email.blank? ? 'xxx@xxx.com' : user_email
+      @author = user_name.strip.gsub('.', '')
+      @author_email = user_email.strip
+      @prefix = 'YK'
       @path = File.expand_path(Dir.getwd.to_s)
     end
 
     def prefix_name
-      return name.start_with?(prefix) ? name : prefix + name
+      name.start_with?(prefix) ? name : prefix + name
     end
   end
-end 
+end
