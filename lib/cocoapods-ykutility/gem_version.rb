ac389c68e37f6dd4360bdbae35e09a35405720a9
coder <coder@gmail.com>
2024-02-03 14:26:41 +0800

    chore: udpate version to 2.0.9

--- lib/cocoapods-ykutility/gem_version.rb
+++ lib/cocoapods-ykutility/gem_version.rb
@@ -1,5 +1,5 @@
 module CocoapodsYkPodUtility
-  VERSION = '2.0.8'
+  VERSION = '2.0.9'
   YK_POD_TEMPLATE_PATH = File.expand_path(File.join(__FILE__, '../../../podTemplate'))
   YK_POD_BUSINESS_REGISTER_PATH = File.expand_path(File.join(__FILE__, '../../../podTemplate/register'))
 
